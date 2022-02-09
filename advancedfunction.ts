12a254ad885e84820a4af30d74bf3363bb6667c9
Prathmesh-codes <prat.babar@gmail.com>
2022-02-09 10:19:29 +0530

    Added feb 08 practice pages

---
+++ advancedfunction.ts
@@ -0,0 +1,11 @@
+//Advanced Functions
+
+//function type
+
+//restricting the parameters to be no and return type as number
+function multiply(p1:number, p2:number): number
+{
+    return p1*p2
+}
+const mul=multiply(10,4)
+console.log(mul)
--- page1.ts
+++ page1.ts
@@ -8,11 +8,6 @@ console.log(`num=${n},type=${typeof n}`)
 const k: number=100
 console.log('k=${k},type=${typeof k}')
 
-
-
-
-
-
 //data type
 const s=10.5
 
@@ -60,8 +55,8 @@ const num='AbraKaDabra'
 console.log(`num = ${num}, data type = ${typeof num}`)
 
 //Checking Equality
-console.log("50"==50)
-console.log('50'===50)
+// console.log("50"==50)
+// console.log('50'===50)
 
 //function
 
@@ -77,3 +72,46 @@ greet() //function call
 
 const numbers=[10,20,'a',15]
 console.log(`numbers = ${numbers}, data type = ${typeof numbers}`)
+
+// implicit object
+const ppl= {
+    name:'ppl1',
+    age:30
+
+}
+console.log(ppl)
+
+//explicit
+const ppl2:object={name:'person2',
+address:'mumbai'}
+
+console.log(ppl2)
+
+//explicit version2
+
+const per3: {name:string;address:string}={
+name:'person3',
+'address':'delhi'
+
+}
+console.log(per3)
+
+//enum
+enum color{
+    RED,
+    GREEN,
+    BLUE
+}
+const red:color=color.RED
+console.log(`color=${color},type=${typeof color}`)
+
+//own datatype that allows string or numbers
+
+type Mytype=string|number
+
+let myVar1:Mytype='test'
+let myVar2:Mytype=12
+
+console.log(myVar1+myVar2)
+
+
